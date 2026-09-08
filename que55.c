a8072de674c3d6b255b289a2d5b57823122caa15
divyayanshh <divyansh8115366833@gmail.com>
2026-09-09 02:44:03 +0530

    Add files via upload

---
+++ que55.c
@@ -0,0 +1,31 @@
+// Write a C program to print all prime numbers from 1 to n.
+
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isPrime;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Prime numbers from 1 to %d are:\n", n);
+
+    for (i = 2; i <= n; i++) {
+        isPrime = 1;  // assume i is prime
+
+        // check divisibility
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                isPrime = 0;  // not prime
+                break;
+            }
+        }
+
+        if (isPrime == 1) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
