73c0488a6acfcd837302a67f0afbcb636663d981
divyayanshh <divyansh8115366833@gmail.com>
2026-09-13 20:57:24 +0530

    Add files via upload

---
+++ que69.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {12, 35, 1, 10, 34, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int first = -1, second = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    if (second == -1)
+        printf("No second largest element\n");
+    else
+        printf("Second largest element is %d\n", second);
+
+    return 0;
+}
