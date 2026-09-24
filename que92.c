fff33dbbebee728d7f66dd29362a5fdebe970921
divyayanshh <divyansh8115366833@gmail.com>
2026-09-24 21:37:35 +0530

    Add files via upload

---
+++ que92.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int freq[26] = {0};  // frequency array for lowercase letters
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            freq[str[i] - 'a']++;
+            if (freq[str[i] - 'a'] == 2) {  // second occurrence
+                printf("First repeating lowercase alphabet: %c\n", str[i]);
+                return 0;
+            }
+        }
+    }
+
+    printf("No repeating lowercase alphabet found.\n");
+    return 0;
+}
